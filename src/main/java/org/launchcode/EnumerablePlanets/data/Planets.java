bf454353535bd852c38cfafbe867271e8576733c
Terri Penn <coderterri@protonmail.com>
2023-10-13 16:48:18 -0500

    Chapter 16: Enums exercises

--- src/main/java/org/launchcode/EnumerablePlanets/data/Planets.java
+++ src/main/java/org/launchcode/EnumerablePlanets/data/Planets.java
@@ -1,21 +1,23 @@
 package org.launchcode.EnumerablePlanets.data;
 
 public enum Planets {
-    MERCURY("Mercury", 88),
-    VENUS("Venus", 225),
-    EARTH("Earth", 365),
-    MARS("Mars", 687),
-    JUPITER("Jupiter", 4333),
-    SATURN("Saturn", 10759),
-    URANUS("Uranus", 30687),
-    NEPTUNE("Neptune", 60200);
+    MERCURY("Mercury", 88, 2440),
+    VENUS("Venus", 225, 6052),
+    EARTH("Earth", 365, 6371),
+    MARS("Mars", 687, 3390),
+    JUPITER("Jupiter", 4333, 69911),
+    SATURN("Saturn", 10759, 58232),
+    URANUS("Uranus", 30687, 25362),
+    NEPTUNE("Neptune", 60200, 24622);
 
     private final String name;
     private final int yearLength;
+    private final int radius;
 
-    Planets(String name, int yearLength) {
+    Planets(String name, int yearLength, int radius) {
         this.name = name;
         this.yearLength = yearLength;
+        this.radius = radius;
     }
 
     public String getName() {
@@ -24,5 +26,9 @@ public enum Planets {
 
     public int getYearLength() {
         return yearLength;
+    }
+
+    public int getRadius() {
+        return radius;
     }
 }
